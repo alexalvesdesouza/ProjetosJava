248f1ae4afc3a51e925d15e18c6370ad25c91cf6
JavaProjects <alexalvesdesouza@outlook.com>
2015-06-04 15:27:51 -0300

    Primeiro commit

---
+++ HelloWord/src/main/java/br/com/modelo/HelloWord.java
@@ -0,0 +1,12 @@
+package br.com.modelo;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Word!!");
+
+	}
+
+}
